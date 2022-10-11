bedce72473badefc3b3c2a18adf95c3186000bcd
mvitoriaferreira <vitoriaaferreira3@gmail.com>
2022-10-11 11:51:56 -0300

    updates sem4

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,24 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
-import { PROMOTIONS } from '../shared/promotions';
-import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() { }
+  constructor(public http: HttpClient,
+    public processHTTPMsgService: ProcessHTTPMsgService) { }
 
   getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + 'promotions')
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
-  getPromotion(id: string): Observable<Promotion> {
-    return of(PROMOTIONS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+  getPromotion(id: number): Observable<Promotion> {
+    return this.http.get<Promotion>(baseURL + 'promotions/' + id)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promo) => promo.featured)[0]).pipe(delay(2000));
+    return this.http.get<Promotion[]>(baseURL + 'promotions?featured=true').pipe(map(promotions => promotions[0]))
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+
+  getPromotionIds(): Observable<number[] | any> {
+    return this.getPromotions().pipe(map(promotions => promotions.map(promotions => promotions.id)))
+      .pipe(catchError(error => error));
   }
 }
